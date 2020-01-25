1b98da186d266172fc5f5141c3df3bbe12f33d63
sam-sepi <sam-sepi.84@gmail.com>
2020-01-25 15:21:51 +0100

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -26,3 +26,16 @@ targets = [21, 22, 25, 80, 8080, 110, 143, 156, 194, 443]
 
 for port in targets:
     scanner('hackthissite.org', port, 1)
+
+"""
+21 is Filtered
+22 is Filtered
+25 is Filtered
+80 is Open
+8080 is Filtered
+110 is Filtered
+143 is Filtered
+156 is Filtered
+194 is Filtered
+443 is Open
+"""
