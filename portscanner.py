53ea7d472902d18998ec4ba1053245b0da87b1d4
sam-sepi <sam-sepi.84@gmail.com>
2020-01-26 16:17:47 +0100

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -4,6 +4,8 @@ import socket
 def scanner(address, port, delay):
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    #Set a timeout on blocking socket operations
     s.settimeout(delay)
    
     try:
@@ -13,29 +15,31 @@ def scanner(address, port, delay):
             print(str(port) + ' is Open')
             s.close()
         
-        else:
+        elif ret == 10061:
             print(str(port) + ' is Filtered')
+            s.close()
+        
+        else:
+            print(str(port) + ' is Closed')
             s.close()
 
     except Exception:
         pass
 
-
 targets = [21, 22, 25, 80, 8080, 110, 143, 156, 194, 443]
 
-
 for port in targets:
-    scanner('hackthissite.org', port, 1)
+    scanner('hackthissite.org', port, 5)
 
 """
-21 is Filtered
+21 is Closed
 22 is Filtered
-25 is Filtered
+25 is Closed
 80 is Open
-8080 is Filtered
-110 is Filtered
-143 is Filtered
-156 is Filtered
-194 is Filtered
+8080 is Closed
+110 is Closed
+143 is Closed
+156 is Closed
+194 is Closed
 443 is Open
 """
